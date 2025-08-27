e1ecb55960c3faf8e56393b4f19c12bbec76bfe8
Omar Hamad <etatldr@gmail.com>
2025-08-27 14:46:30 +0300

    Update Nginx configuration for Dcm4chee and Keycloak

--- platform/app/.recipes/Nginx-Dcm4chee-Keycloak/config/docker-nginx-dcm4chee-keycloak.js
+++ platform/app/.recipes/Nginx-Dcm4chee-Keycloak/config/docker-nginx-dcm4chee-keycloak.js
@@ -1,31 +1,41 @@
-/** @type {AppTypes.Config} */
 window.config = {
-  routerBasename: '/ohif-viewer/',
+  routerBasename: "/",
   showStudyList: true,
   extensions: [],
   modes: [],
-  // below flag is for performance reasons, but it might not work for all servers
   showWarningMessageForCrossOrigin: true,
   showCPUFallbackMessage: true,
   showLoadingIndicator: true,
   strictZSpacingForVolumeViewport: true,
-  defaultDataSourceName: 'dicomweb',
+  oidc: [
+    {
+      authority: "https://authdicom.cloudcompuexpediente.com/realms/dcm4che",
+      client_id: "ohif-viewer",
+      redirect_uri: "https://viewerdicom.cloudcompuexpediente.com/callback",
+      response_type: "id_token token",
+      scope: "openid email profile",
+      post_logout_redirect_uri: "/",
+      automaticSilentRenew: true,
+      revokeAccessTokenOnSignout: true
+    }
+  ],
+  defaultDataSourceName: "dicomweb",
   dataSources: [
     {
-      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-      sourceName: 'dicomweb',
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomweb",
+      sourceName: "dicomweb",
       configuration: {
-        friendlyName: 'Dcm4chee Server',
-        name: 'Dcm4chee',
+        friendlyName: "Dcm4chee Server",
+        name: "Dcm4chee",
         wadoUriRoot: "https://pacsdicom.cloudcompuexpediente.com/dcm4chee-arc/aets/DCM4CHEE/wado",
         qidoRoot: "https://pacsdicom.cloudcompuexpediente.com/dcm4chee-arc/aets/DCM4CHEE/rs",
         wadoRoot: "https://pacsdicom.cloudcompuexpediente.com/dcm4chee-arc/aets/DCM4CHEE/rs",
         qidoSupportsIncludeField: false,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
+        imageRendering: "wadors",
+        thumbnailRendering: "wadors",
         dicomUploadEnabled: true,
-        omitQuotationForMultipartRequest: true,
-      },
-    },
-  ],
+        omitQuotationForMultipartRequest: true
+      }
+    }
+  ]
 };
